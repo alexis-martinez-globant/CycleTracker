b215923c03bb2f5a99452c5d722ab5a1b56d607b
Alexis Martinez Degiuda <alexis.martinez@smiledirectclub.com>
2024-01-08 13:05:01 -0300

    1st commit

---
+++ service-worker.js
@@ -0,0 +1,66 @@
+//! control version
+const VERSION = "v1";
+
+//! cache version
+const CACHE_NAME = `period-tracker-${VERSION}`;
+
+//! cache needed resources 
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/cycletrack.json",
+  "/icons/wheel.svg",
+];
+
+//! listener that retrieve and stores the files listed on install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+//! updating and deleting old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
+//! fetching, intercepting server requests and responf with cache
+self.addEventListener("fetch", (event) => {
+  // when seeking an HTML page
+  if (event.request.mode === "navigate") {
+    // Return to the index.html page
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For every other request type
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // Respond with a HTTP 404 response status.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
